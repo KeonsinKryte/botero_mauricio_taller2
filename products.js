647bea3f0ad2e43203252818488c951d9c6ec579
KeonsinKryte <mauricioboterovallejo32@gmail.com>
2018-10-24 00:27:18 -0500

    Loading Products sources/description of each product

---
+++ products.js
@@ -0,0 +1,146 @@
+db.getCollection('Products').insertMany([
+    {
+        product__name: "¡UNO!",
+        product__songs:
+            [
+                {
+                    song__number: 1,
+                    song__name: "Nuclear Family",
+                    song__lenght: "3:03"
+                },
+                {
+                    song__number: 2,
+                    song__name: "Stay the Night",
+                    song__lenght: "4:36"
+                },
+                {
+                    song__number: 3,
+                    song__name: "Carpe Diem",
+                    song__lenght: "3:25"
+                },
+                {
+                    song__number: 4,
+                    song__name: "Let Yourself Go",
+                    song__lenght: "2:56"
+                },
+                {
+                    song__number: 5,
+                    song__name: "Kill the DJ",
+                    song__lenght: "3:44"
+                },
+                {
+                    song__number: 6,
+                    song__name: "Fell for You",
+                    song__lenght: "3:08"
+                },
+                {
+                    song__number: 7,
+                    song__name: "Loss of Control",
+                    song__lenght: "3:07"
+                },
+                {
+                    song__number: 8,
+                    song__name: "Troublemaker",
+                    song__lenght: "2:45"
+                },
+                {
+                    song__number: 9,
+                    song__name: "Angel Blue",
+                    song__lenght: "2:46"
+                },
+                {
+                    song__number: 10,
+                    song__name: "Sweet 16",
+                    song__lenght: "3:03"
+                },
+                {
+                    song__number: 11,
+                    song__name: "Rusty James",
+                    song__lenght: "4:09"
+                },
+                {
+                    song__number: 12,
+                    song__name: "Oh Love",
+                    song__lenght: "5:03"
+                },
+            ],
+
+        product__imgCover: "..\sources\images\Products\Green Day\I.¡UNO! - Cover.jpg",
+        product__imgLP: "",
+        product__year: "2012",
+        product__genre: "Punk",
+        product__band: "Green Day",
+        product__price: 9.49
+    },
+    {
+        product__name: "¡DOS!",
+        product__songs:
+            [
+                {
+                    song__number: 1,
+                    song__name: "Nuclear Family",
+                    song__lenght: "3:03"
+                },
+                {
+                    song__number: 2,
+                    song__name: "Stay the Night",
+                    song__lenght: "4:36"
+                },
+                {
+                    song__number: 3,
+                    song__name: "Carpe Diem",
+                    song__lenght: "3:25"
+                },
+                {
+                    song__number: 4,
+                    song__name: "Let Yourself Go",
+                    song__lenght: "2:56"
+                },
+                {
+                    song__number: 5,
+                    song__name: "Kill the DJ",
+                    song__lenght: "3:44"
+                },
+                {
+                    song__number: 6,
+                    song__name: "Fell for You",
+                    song__lenght: "3:08"
+                },
+                {
+                    song__number: 7,
+                    song__name: "Loss of Control",
+                    song__lenght: "3:07"
+                },
+                {
+                    song__number: 8,
+                    song__name: "Troublemaker",
+                    song__lenght: "2:45"
+                },
+                {
+                    song__number: 9,
+                    song__name: "Angel Blue",
+                    song__lenght: "2:46"
+                },
+                {
+                    song__number: 10,
+                    song__name: "Sweet 16",
+                    song__lenght: "3:03"
+                },
+                {
+                    song__number: 11,
+                    song__name: "Rusty James",
+                    song__lenght: "4:09"
+                },
+                {
+                    song__number: 12,
+                    song__name: "Oh Love",
+                    song__lenght: "5:03"
+                },
+            ],
+
+        product__img: "public\sources\images\Products\Green Day\I.¡UNO! - Cover.jpg",
+        product__year: "2012",
+        product__genre: "Punk",
+        product__band: "Green Day"
+    },
+]);
