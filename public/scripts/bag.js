0d39a94c7f83d50488a3801201b21fe6ddbbb493
KeonsinKryte <mauricioboterovallejo32@gmail.com>
2018-10-31 00:00:25 -0500

    Linked and added to checkout

--- public/scripts/bag.js
+++ public/scripts/bag.js
@@ -12,32 +12,61 @@ window.addEventListener('load', function () {
         });
         document.querySelector('.bag__total').innerText = shoppingCart.length;
         shoppingCart.forEach(function (elem) {
-            document.querySelector('.bag__items').innerHTML +=
-                `<article class="bag__product">
-                    <div class="bag__cover">
-                        <img src="${elem.product__cover}" alt="product cover">
-                    </div>
-                    <div class="bag__text">
-                        <p>${elem.product__name}</p>
-                        <h6>${elem.product__price}</h6>
-                    </div>
-                </article>`;
-
+            bag(elem);
         });
+    }
+
+    function bag(elem) {
+        var article = document.createElement('article');
+        article.setAttribute('class', 'bag__products');
+
+        var div = document.createElement('div');
+        div.setAttribute('class', 'bag__cover');
+
+        var div2 = document.createElement('div');
+        div2.setAttribute('class', 'bag__text');
+
+        var img = document.createElement('img');
+        img.setAttribute('src', elem.product__cover);
+
+        console.log(elem.product__cover);
+
+        var p = document.createElement('p');
+        p.innerText = elem.product__name;
+
+        console.log(elem.product__name);
+
+        var h6 = document.createElement('h6');
+        h6.innerText = elem.product__price;
+
+        console.log(elem.product__price);
+
+        div2.appendChild(p);
+        div2.appendChild(h6);
+        div.appendChild(img);
+        article.appendChild(div);
+        article.appendChild(div2);
+
+
+
+        // document.querySelector('.bag__items').innerHTML = null;
+        
+        document.querySelector('.bag__items').append(article);
+        console.log(article);
     }
 
     shoppingCart__render();
 
-    document.querySelectorAll('.product .product__button').forEach(function (elem) {
+    document.querySelectorAll('.product button').forEach(function (elem) {
         elem.addEventListener('click', function (event) {
             event.preventDefault();
-            let parent = document.querySelector('.product__info');
+            let parent = elem.parentElement;
 
             let product = {
-                product__name: parent.querySelector(".productname").innerHTML,
-                product__artist: parent.querySelector(".productartist").innerHTML,
-                product__price: parent.querySelector(".productprice").innerHTML,
-                product__cover: '/sources/images/products/' + parent.querySelector(".productartist").innerHTML + '/I.' + parent.querySelector(".productname").innerHTML + ' - Cover.jpg'
+                product__name: parent.querySelector('h5').innerText,
+                product__artist: parent.querySelector('h2').innerText,
+                product__price: parent.querySelector('h6').innerText,
+                product__cover: '/sources/images/products/' + parent.querySelector('h2').innerText + '/I.' + parent.querySelector('h5').innerText + ' - Cover.jpg'
             };
 
             shoppingCart.push(product);
